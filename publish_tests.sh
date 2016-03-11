5689fbdd41cfa1bfffcaed9cd0a2b7ec47166395
Micah Silverman <micah@afitnerd.com>
2016-03-11 15:37:01 -0500

    update

--- publish_tests.sh
+++ publish_tests.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
 
-echo "PUBLISH TESTS"
+echo "Cloning Heroku repo"
+git clone https://git.heroku.com/whispering-cliffs-74141.git
+
+echo "Updating Heroku app"
+cd whispering-cliffs-74141
+echo "New Build" > home.html
+echo `date` >> home.html
+
+echo "commit and push Heroku app"
+git add -A .
+git commit -m "update report"
+git push origin master
+
